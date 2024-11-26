f14a0893bdf2eac399a7c4fff2926ea642a027e7
Leonardo Wiltzki Lourenço <129564287+LEONARDO-WIL-WOK@users.noreply.github.com>
2024-11-26 19:00:19 -0300

    Teste com sucesso

--- src/test/java/testsHelper/HRoadsTest.java
+++ src/test/java/testsHelper/HRoadsTest.java
@@ -23,7 +23,8 @@ public class HRoadsTest {
     public void validarCadastroDeOficinaRepetida() {
         metodosHelper sistema = new metodosHelper();
         sistema.registrarOficina("Auto Center Y");
-//        sistema.registrarOficina("Auto Center Y");
+//        Esta linha a baixo
+        sistema.registrarOficina("Auto Center Y");
         List<String> oficinas = sistema.obterOficinas();
         assertEquals(2, oficinas.size());
     }
